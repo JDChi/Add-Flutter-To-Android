e5a9493687286f31b9d6baaf398988c975c229c4
jdchi <623893416@qq.com>
2021-06-10 18:19:50 +0800

    补充销毁 FlutterEngine 注释

--- app/src/main/java/com/example/add_flutter_to_android/MyApplication.java
+++ app/src/main/java/com/example/add_flutter_to_android/MyApplication.java
@@ -20,5 +20,10 @@ public class MyApplication extends Application {
         // 尤其是入口方法里调用 runApp()，此时 Flutter 引用开始运行在一个大小为0的窗口里
         flutterEngine.getDartExecutor().executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault());
         FlutterEngineCache.getInstance().put(CacheId.MY_ENGINE_ID, flutterEngine);
+
+        // 当使用一个缓存的 FlutterEngine 时， FlutterEngine 会比展示它的 FlutterActivity 或 FlutterFragment 存活得更久。
+        // 要停止代码运行和清理相关资源，可以从 FlutterEngineCache 中获取你的 FlutterEngine，然后使用 FlutterEngine.destroy() 来销毁 FlutterEngine。
+        // FlutterEngine cacheEngine = FlutterEngineCache.getInstance().get(CacheId.MY_ENGINE_ID);
+        // cacheEngine.destroy();
     }
 }
