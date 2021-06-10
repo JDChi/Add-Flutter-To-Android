3025a60920baaa2147f08904f404de48d598877f
jdchi <623893416@qq.com>
2021-06-10 14:36:35 +0800

    跳转到 FlutterActivity

--- app/src/main/java/com/example/add_flutter_to_android/MainActivity.java
+++ app/src/main/java/com/example/add_flutter_to_android/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.add_flutter_to_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton bt_flutter_activity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_flutter_activity = findViewById(R.id.bt_flutter_activity);
+        bt_flutter_activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(FlutterActivity.createDefaultIntent(MainActivity.this));
+            }
+        });
     }
 }
